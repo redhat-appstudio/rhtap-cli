1f80bf44f8d9c76a772d3b4567696c4bad9720b5
Otavio Fernandes <otaviof@gmail.com>
2024-03-17 17:59:05 +0100

    refator(docs): Minor Doc Comments

--- charts/rhtap-subscriptions/hack/find-os-subscription.sh
+++ charts/rhtap-subscriptions/hack/find-os-subscription.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+#
+# Finds the subscription for the given catalog name, shows the information needed
+# to add the OpenShift operator subscription on this chart.
+#
 
+# OpenShift operator catalog name, for instance "openshift-pipelines-operator-rh".
 declare -r CATALOG_NAME="${1:-}"
 
 if [[ -z "${CATALOG_NAME}" ]]; then
@@ -7,16 +12,21 @@ if [[ -z "${CATALOG_NAME}" ]]; then
     exit 1
 fi
 
+# Inspecting the manifests installed on the cluster, looking for the catalog name
+# informed. The availability of the operators depends on the OpenShift cluster
+# version.
 if ! oc get packagemanifests "${CATALOG_NAME}" >/dev/null; then
     echo "# ERROR: No subscription found for '${CATALOG_NAME}'!"
     exit 1
 fi
 
+# Extracing the source name, and the namespace where the catalog is available
 source=$(oc get packagemanifests "${CATALOG_NAME}" \
     --output="jsonpath={.status.catalogSource}")
 namespace=$(oc get packagemanifests "${CATALOG_NAME}" \
     --output="jsonpath={.status.catalogSourceNamespace}")
 
+# Listing the channels and the startingCSVs available for the catalog.
 mapfile -t channels < <(echo -en "$(oc get packagemanifests "${CATALOG_NAME}" \
     --output jsonpath='{range .status.channels[*]}{.name}\n{end}')")
 mapfile -t startingCSVs < <(echo -en "$(oc get packagemanifests "${CATALOG_NAME}" \

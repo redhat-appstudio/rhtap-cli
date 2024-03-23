12de92ac108cf64b09b6a3148bae424112b5b246
Otavio Fernandes <otaviof@gmail.com>
2024-03-23 08:00:25 +0100

    feat(pkg): Global Flags

---
+++ pkg/flags/installer.go
@@ -0,0 +1,11 @@
+package flags
+
+import "github.com/spf13/pflag"
+
+// ValuesTemplateFlag flag name for the values template file.
+const ValuesTemplateFlag = "values-template"
+
+// SetValuesTmplFlag sets up the values-template flag to the informed pointer.
+func SetValuesTmplFlag(p *pflag.FlagSet, v *string) {
+	p.StringVar(v, ValuesTemplateFlag, *v, "Path to the values template file")
+}

a36a760aa434316daf6e1da6c8731fd984ddb1c3
Romain Arnaud <rarnaud@redhat.com>
2024-06-07 16:56:12 -0400

    fix: correct secret creation order

--- charts/rhtap-backing-services/hooks/post-deploy.sh
+++ charts/rhtap-backing-services/hooks/post-deploy.sh
@@ -3,28 +3,6 @@
 shopt -s inherit_errexit
 set -Eeu -o pipefail
 
-app_namespaces() {
-    ### Workaround
-    ### Helm does not support the patching of resources.
-    echo -n "* Patching ServiceAccounts in rhtap-app-*: "
-    for env in "development" "prod" "stage"; do
-        for SA in "default" "pipeline"; do
-            until kubectl get serviceaccounts --namespace "rhtap-app-$env" "$SA" >/dev/null 2>&1; do
-                echo -n "_"
-                sleep 2
-            done
-            echo -n "."
-            kubectl patch serviceaccounts --namespace "rhtap-app-$env" "$SA" --patch "
-secrets:
-    - name: quay-auth
-imagePullSecrets:
-    - name: quay-auth
-" >/dev/null
-        done
-    done
-    echo "OK"
-}
-
 openshift_gitops() {
     NAMESPACE="rhtap"
     RHTAP_ARGOCD_INSTANCE="$NAMESPACE-argocd"
@@ -82,7 +60,6 @@ openshift_gitops() {
 main() {
     TEMP_DIR="$(mktemp -d)"
     cd "$TEMP_DIR"
-    app_namespaces
     openshift_gitops
     cd - >/dev/null
     rm -rf "$TEMP_DIR"

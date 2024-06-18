f2065c5cef668fb4751fcfe039a6e2c7fb2d9e78
Otavio Fernandes <otaviof@gmail.com>
2024-06-18 09:51:28 +0200

    feat: Hooks Support for ChartFS
    Modifying Hooks to support ChartFS, as in, reading files from the
    `io.FS` interface. Improved testing to detect the hook scripts have
    actually been executed.

--- pkg/hooks/hooks.go
+++ pkg/hooks/hooks.go
@@ -2,50 +2,96 @@ package hooks
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path"
 
+	"github.com/redhat-appstudio/rhtap-cli/pkg/chartfs"
 	"github.com/redhat-appstudio/rhtap-cli/pkg/config"
-	"helm.sh/helm/v3/pkg/chartutil"
 )
 
+// Hooks represent the hooks that can be executed before and after the Helm Chart
+// installation, it provides the user the ability to customize the process using
+// shell scripts. These scripts can rely on local tools, like "kubectl", "oc" and
+// others, while the Helm Charts are only using Kubernetes resources.
+// Ideally these scripts are temporary measures, and should be replaced by Helm
+// Chart related resources as soon as possible.
 type Hooks struct {
-	dep config.Dependency
+	cfs    *chartfs.ChartFS   // chart file system
+	dep    *config.Dependency // helm chart dependency
+	stdout io.Writer          // standard output
+	stderr io.Writer          // standard error
 }
 
-func (h *Hooks) runHookScript(name string, vals chartutil.Values) error {
+// exec executes the script with the given environment variables.
+func (h *Hooks) exec(scriptPath string, vals map[string]interface{}) error {
+	cmd := exec.Command(scriptPath)
+	cmd.Env = os.Environ()
+	for k, v := range vals {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%v", k, v))
+	}
+	cmd.Stdout = h.stdout
+	cmd.Stderr = h.stderr
+	return cmd.Run()
+}
+
+// runHookScript executes the hook script with the given values.
+func (h *Hooks) runHookScript(name string, vals map[string]interface{}) error {
+	// Extracting the script payload from the Chart directory, using the "hook"
+	// directory as default location.
 	scriptPath := path.Join(h.dep.Chart, "hooks", name)
-	_, err := os.Stat(scriptPath)
+	scriptBytes, err := h.cfs.ReadFile(scriptPath)
 	if err != nil {
+		// Ignoring when the script is not found, it means the given Chart does
+		// not carry hook scripts.
 		if os.IsNotExist(err) {
 			return nil
 		}
 		return err
 	}
 
-	cmd := exec.Command(scriptPath)
-	cmd.Env = os.Environ()
-	for k, v := range vals {
-		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%v", k, v))
+	// Storing the script payload in a temporary file, adding permissions to
+	// execute as a regular shell script.
+	tmpfile, err := os.CreateTemp("/tmp", "rhtap-cli-hook-*.sh")
+	if err != nil {
+		return err
 	}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
+	defer os.Remove(tmpfile.Name())
+	if _, err := tmpfile.Write(scriptBytes); err != nil {
+		return err
+	}
+	if err := tmpfile.Close(); err != nil {
+		return err
+	}
+	if err := os.Chmod(tmpfile.Name(), 0o755); err != nil {
 		return err
 	}
 
-	return nil
+	return h.exec(tmpfile.Name(), vals)
 }
 
-func (h *Hooks) PreDeploy(vals chartutil.Values) error {
+// PreDeploy executes the "pre-deploy.sh" hook script with the given values.
+func (h *Hooks) PreDeploy(vals map[string]interface{}) error {
 	return h.runHookScript("pre-deploy.sh", vals)
 }
 
-func (h *Hooks) PostDeploy(vals chartutil.Values) error {
+// PostDeploy executes the "post-deploy.sh" hook script with the given values.
+func (h *Hooks) PostDeploy(vals map[string]interface{}) error {
 	return h.runHookScript("post-deploy.sh", vals)
 }
 
-func NewHooks(dep config.Dependency) *Hooks {
-	return &Hooks{dep: dep}
+// NewHooks instantiates a hooks handler for the given ChartFS and Dependency.
+func NewHooks(
+	cfs *chartfs.ChartFS,
+	dep *config.Dependency,
+	stdout io.Writer,
+	stderr io.Writer,
+) *Hooks {
+	return &Hooks{
+		cfs:    cfs,
+		dep:    dep,
+		stdout: stdout,
+		stderr: stderr,
+	}
 }

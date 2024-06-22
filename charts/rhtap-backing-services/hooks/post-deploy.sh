c8d7f622cae1fae016042bbdebc7e584db53a09b
Otavio Fernandes <otaviof@gmail.com>
2024-06-22 08:06:01 +0200

    feat: ArgoCD Login and Token Generation
    A single script to both test the ArgoCD login and generate a token for
    use in the ArgoCD API.

--- charts/rhtap-backing-services/hooks/post-deploy.sh
+++ charts/rhtap-backing-services/hooks/post-deploy.sh
@@ -1,68 +1,123 @@
 #!/usr/bin/env bash
+#
+# Tests if the ArgoCD instance is available on the cluster by logging in.
+#
+# Uses the ArgoCD session, created by previsoly running "argocd login", to
+# generate a acccount token. The informaton is then stored on the ARGOCD_ENV_FILE
+# variable.
+#
 
 shopt -s inherit_errexit
 set -Eeu -o pipefail
 
-openshift_gitops() {
-    NAMESPACE="rhtap"
-    RHTAP_ARGOCD_INSTANCE="$NAMESPACE-argocd"
+declare -r SUBCOMMAND="${1:-}"
 
-    echo -n "* Installing 'argocd' CLI: "
-    curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-    chmod 555 argocd
-    ./argocd version --client | head -1 | cut -d' ' -f2
+# ArgoCD hostname (FQDN) to test.
+declare -r ARGOCD_HOSTNAME="${ARGOCD_HOSTNAME:-}"
+# ArgoCD username to use for login.
+declare -r ARGOCD_USER="${ARGOCD_USER:-admin}"
+# ArgoCD password to use for login.
+declare -r ARGOCD_PASSWORD="${ARGOCD_PASSWORD:-}"
+# Environment file to store the ArgoCD crednetials.
+declare -r ARGOCD_ENV_FILE="${ARGOCD_ENV_FILE:-/rhtap/argocd/env}"
 
-    echo -n "* Wait for ArgoCD instance: "
-    until kubectl get route -n "$NAMESPACE" "$RHTAP_ARGOCD_INSTANCE-server" >/dev/null 2>&1; do
-        echo -n "_"
-        sleep 2
+fail() {
+    echo "# [ERROR] ${*}" >&2
+    exit 1
+}
+
+# Asserts the required environment variables.
+assert_variables() {
+    [[ -z "${ARGOCD_HOSTNAME}" ]] &&
+        fail "ARGOCD_HOSTNAME is not set!"
+    [[ -z "${ARGOCD_USER}" ]] &&
+        fail "ARGOCD_USER is not set!"
+    [[ -z "${ARGOCD_PASSWORD}" ]] &&
+        fail "ARGOCD_PASSWORD is not set!"
+}
+
+# Executes the ArgoCD login command.
+argocd_login() {
+    argocd login "${ARGOCD_HOSTNAME}" \
+        --grpc-web \
+        --insecure \
+        --http-retry-max="5" \
+        --username="${ARGOCD_USER}" \
+        --password="${ARGOCD_PASSWORD}"
+}
+
+# Retries a few times until the ArgoCD instance is available.
+test_argocd_login() {
+    for i in {1..30}; do
+        echo "# [${i}/30] Testing ArgoCD login on '${ARGOCD_HOSTNAME}'..."
+        argocd_login &&
+            return 0
+
+        wait=$((i * 5))
+        echo "### [${i}/30] Waiting for ${wait} seconds before retrying..."
+        sleep ${wait}
     done
-    echo "OK"
+    return 1
+}
 
-    ### Workaround
-    ### The ArgoCD token cannot be created via a manifest.
-    echo -n "* Configure ArgoCD admin user: "
-    RHTAP_ARGOCD_SECRET="rhtap-argocd-integration"
-    if [ "$(kubectl get secret "$RHTAP_ARGOCD_SECRET" -n "$NAMESPACE" -o name --ignore-not-found | wc -l)" = "0" ]; then
-        ARGOCD_HOSTNAME="$(kubectl get route -n "$NAMESPACE" "$RHTAP_ARGOCD_INSTANCE-server" --ignore-not-found -o jsonpath="{.spec.host}")"
-        echo -n "."
-        ARGOCD_PASSWORD="$(kubectl get secret -n "$NAMESPACE" "$RHTAP_ARGOCD_INSTANCE-cluster" -o jsonpath="{.data.admin\.password}" | base64 --decode)"
-        echo -n "."
-        RETRY=0
-        while ! ./argocd login "$ARGOCD_HOSTNAME" --grpc-web --insecure --http-retry-max 5 --username admin --password "$ARGOCD_PASSWORD" >/dev/null; do
-            if [ "$RETRY" = "20" ]; then
-                echo "FAIL"
-                echo "[ERROR] Could not login to ArgoCD" >&2
-                exit 1
-            else
-                echo -n "_"
-                RETRY=$((RETRY + 1))
-                sleep 5
-            fi
-        done
-        echo -n "."
-        ARGOCD_API_TOKEN="$(./argocd account generate-token --http-retry-max 5 --account "admin")"
-        echo -n "."
-        kubectl create secret generic "$RHTAP_ARGOCD_SECRET" \
-            --namespace="$NAMESPACE" \
-            --from-literal="ARGOCD_API_TOKEN=$ARGOCD_API_TOKEN" \
-            --from-literal="ARGOCD_HOSTNAME=$ARGOCD_HOSTNAME" \
-            --from-literal="ARGOCD_PASSWORD=$ARGOCD_PASSWORD" \
-            --from-literal="ARGOCD_USER=admin" \
-            > /dev/null
+# Generates the ArgoCD API token.
+argocd_generate_token() {
+    echo "# Generating ArgoCD API token on '${ARGOCD_HOSTNAME}'..."
+    ARGOCD_API_TOKEN="$(
+        argocd account generate-token \
+            --grpc-web \
+            --insecure \
+            --http-retry-max="5" \
+            --account="${ARGOCD_USER}"
+    )"
+    if [[ ${?} -ne 0 ]]; then
+        fail "ArgoCD API token could not be generated!"
     fi
-    echo "OK"
+    if [[ -z "${ARGOCD_API_TOKEN}" ]]; then
+        fail "ArgoCD API token could not be generated!"
+    fi
+
+    echo "# Storing ArgoCD API credentials on '${ARGOCD_ENV_FILE}'..."
+    cat <<EOF >"${ARGOCD_ENV_FILE}" || fail "Fail to write '${ARGOCD_ENV_FILE}'!"
+ARGOCD_HOSTNAME="${ARGOCD_HOSTNAME}"
+ARGOCD_USER="${ARGOCD_USER}"
+ARGOCD_PASSWORD="${ARGOCD_PASSWORD}"
+ARGOCD_API_TOKEN="${ARGOCD_API_TOKEN}"
+EOF
+    return 0
 }
 
 #
 # Main
 #
-main() {
-    TEMP_DIR="$(mktemp -d)"
-    cd "$TEMP_DIR"
-    openshift_gitops
-    cd - >/dev/null
-    rm -rf "$TEMP_DIR"
-}
 
-main
+assert_variables &&
+    echo "# All environment variables are set"
+
+case "${SUBCOMMAND}" in
+login)
+    if test_argocd_login; then
+        echo "# ArgoCD is available: '${ARGOCD_HOSTNAME}'"
+        exit 0
+    else
+        fail "ArgoCD not available!"
+    fi
+    ;;
+generate)
+    echo "# Logging into ArgoCD on '${ARGOCD_HOSTNAME}'..."
+    test_argocd_login ||
+        fail "ArgoCD not available!"
+
+    if argocd_generate_token; then
+        echo "# ArgoCD API token generated successfully!"
+        cat "${ARGOCD_ENV_FILE}"
+        exit 0
+    else
+        fail "ArgoCD API token could not be generated!"
+    fi
+    ;;
+*)
+    fail "Invalid subcommand provided: '${SUBCOMMAND}'. " \
+        "Use 'login' or 'generate'!"
+    ;;
+esac

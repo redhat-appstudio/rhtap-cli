redhat-appstudio/rhtap-cli

commit 44728b595a9d9e2b9c04a93e49b2e247d0c45836

feat: deploy dh and gitops in their own namespaces
There's no good reason for the 2 services to share the same namespace.
This change helps with segregation of services. This in turn helps with
development, as it guarantee that there is no unknown link between the
2 services and one can reset a service by deleting the namespace and
redeploying the cluster.

With this change, the `rhtap` namespace becomes a location for
integration secrets.

rh-pre-commit.version: 2.3.2
rh-pre-commit.check-secrets: ENABLED

installer/charts/rhtap-gitops/values.yaml
@@ -9,9 +9,10 @@ argoCD:
   namespace: __OVERWRITE_ME__
   # The domain for the ArgoCD instance, used to define the final route.
   ingressDomain: __OVERWRITE_ME__
-  # The secret name to store the ArgoCD API credentials, this secret is later on
+  # The secret namespace to store the ArgoCD API credentials, this secret is later on
   # used for integration with other services.
-  secretName: __OVERWRITE_ME__
+  integrationSecret:
+    namespace: __OVERWRITE_ME__
   # Route configuration for all ArgoCD components.
   route:
     # Toggles the route for the ArgoCD components.

installer/charts/values.yaml.tpl
@@ -31,6 +31,9 @@ openshift:
     - rhacs-operator
     {{- end }}
 {{- end }}
+{{- if $gitops.Enabled }}
+    - {{ $gitops.Namespace }}
+{{- end }}
 {{- if $quay.Enabled }}
     - {{ $quay.Namespace }}
 {{- end }}
@@ -49,8 +52,6 @@ openshift:
 # rhtap-subscriptions
 #
 
-{{- $argoCDNamespace := .Installer.Namespace }}
-
 subscriptions:
   amqStreams:
     enabled: {{ $tpa.Enabled }}
@@ -62,7 +63,7 @@ subscriptions:
     enabled: {{ $gitops.Enabled }}
     managed: {{ and $gitops.Enabled $gitops.Properties.manageSubscription }}
     config:
-      argoCDClusterNamespace: {{ $argoCDNamespace }}
+      argoCDClusterNamespace: {{ $gitops.Namespace }}
   openshiftKeycloak:
     enabled: {{ $keycloak.Enabled }}
     managed: {{ and $keycloak.Enabled $keycloak.Properties.manageSubscription }}
@@ -103,6 +104,8 @@ minIOOperator:
 {{- $quayMinIOSecretName := "quay-minio-root-user"  }}
 
 infrastructure:
+  developerHub:
+    namespace: {{ $rhdh.Namespace }}
   kafkas:
     tpa:
       enabled: {{ $tpa.Enabled }}
@@ -142,7 +145,7 @@ infrastructure:
     patchClusterTektonConfig:
       annotations:
         meta.helm.sh/release-name: rhtap-backing-services
-        meta.helm.sh/release-namespace: {{ $argoCDNamespace }}
+        meta.helm.sh/release-namespace: {{ .Installer.Namespace }}
       labels:
         app.kubernetes.io/managed-by: Helm
 
@@ -152,7 +155,7 @@ infrastructure:
 
 {{- $keycloakRouteTLSSecretName := "keycloak-tls" }}
 {{- $keycloakRouteHost := printf "sso.%s" $ingressDomain }}
-{{- $argoCDName := "argocd" }}
+{{- $argoCDName := printf "%s-gitops" .Installer.Namespace }}
 {{- $quayMinIOHost := printf "minio-%s.%s" $quay.Namespace $ingressDomain }}
 
 backingServices:
@@ -178,11 +181,11 @@ backingServices:
       annotations:
         service.beta.openshift.io/serving-cert-secret-name: {{ $keycloakRouteTLSSecretName }}
   argoCD:
-    enabled: {{ $rhdh.Enabled }}
+    enabled: {{ $gitops.Enabled }}
     name: {{ $argoCDName }}
-    namespace: {{ $argoCDNamespace }}
-    # TODO: link this secret name with RHDH configuration.
-    secretName: rhtap-argocd-integration
+    namespace: {{ $gitops.Namespace }}
+    integrationSecret:
+      namespace: {{ .Installer.Namespace }}
     ingressDomain: {{ $ingressDomain }}
 
 #
@@ -201,14 +204,23 @@ acs: &acs
 acsTest: *acs
 
 #
+# rhtap-app-namespaces
+#
+appNamespaces:
+  argoCD:
+    name: {{ $argoCDName }}
+
+#
 # rhtap-gitops
 #
 
 argoCD:
   enabled: {{ $rhdh.Enabled }}
   name: {{ $argoCDName }}
-  namespace: {{ $argoCDNamespace }}
-  secretName: rhtap-argocd-integration
+  namespace: {{ $gitops.Namespace }}
+  integrationSecret:
+    name: rhtap-argocd-integration
+    namespace: {{ .Installer.Namespace }}
   ingressDomain: {{ $ingressDomain }}
 
 #
@@ -277,6 +289,8 @@ developerHub:
   namespace: {{ $rhdh.Namespace }}
   ingressDomain: {{ $ingressDomain }}
   catalogURL: {{ $catalogURL }}
+  integrationSecrets:
+    namespace: {{ .Installer.Namespace }}
 
 #
 # rhtap-tpa

installer/charts/rhtap-app-namespaces/values.yaml
@@ -1,4 +1,4 @@
 ---
-argoCD:
-  # ArgoCD instance name controlling the namespace
-  name: __OVERWRITE_ME__
+appNamespaces:
+  argoCD:
+    name: __OVERWRITE_ME__

installer/charts/rhtap-dh/values.yaml
@@ -6,3 +6,5 @@ developerHub:
   instanceName: developer-hub
   catalogURL: __OVERWRITE_ME__
   ingressDomain: __OVERWRITE_ME__
+  integrationSecrets:
+    namespace: __OVERWRITE_ME__

installer/charts/rhtap-app-namespaces/hooks/post-deploy.sh
@@ -21,6 +21,8 @@ patch_serviceaccount() {
     local NAMESPACE="$1"
     local SA="$2"
 
+    echo -n "- Patching ServiceAccount '$SA' in '$NAMESPACE': "
+
     # Wait until the ServiceAccount is available and get the definition
     until "$KUBECTL" get serviceaccounts --namespace "$NAMESPACE" "$SA" >/dev/null 2>&1; do
         echo -n "_"
@@ -44,14 +46,12 @@ patch_serviceaccount() {
         fi
     done
 
-    if [ -e "$SA_DEFINITION_UPDATED" ]; then
-        echo -n "- Patching ServiceAccount '$SA' in '$NAMESPACE': "
-        "$KUBECTL" apply -f "$SA_DEFINITION_UPDATED"
-    fi
+    echo "OK"
+    "$KUBECTL" apply -f "$SA_DEFINITION_UPDATED"
 }
 
 app_namespaces() {
-    NAMESPACE="$INSTALLER__DEVELOPERHUB__NAMESPACE"
+    NAMESPACE="$INSTALLER__QUAY__SECRET__NAMESPACE"
 
     for env in "development" "prod" "stage"; do
         for SA in "default" "pipeline"; do

installer/charts/rhtap-app-namespaces/templates/namespaces.yaml
@@ -1,11 +1,11 @@
 {{ $namespace := .Release.Namespace }}
-{{ $argoCD := .Values.argoCD }}
+{{ $argoCD := .Values.appNamespaces.argoCD.name }}
 {{- range tuple "development" "prod" "stage" }}
 ---
 apiVersion: v1
 kind: Namespace
 metadata:
   labels:
-    argocd.argoproj.io/managed-by: {{ $argoCD.name }}
+    argocd.argoproj.io/managed-by: {{ $argoCD }}
   name: {{ $namespace }}-app-{{ . }}
 {{- end }}

installer/charts/rhtap-dh/templates/app-config-content.yaml
@@ -1,18 +1,19 @@
 {{- define "rhtap-dh.app-conf" }}
+{{- $integrationNamespace := .Values.developerHub.integrationSecrets.namespace }}
 app:
   title: Red Hat Developer Hub
   baseUrl: ${BACKEND_URL}
 # Lookup for all the required secrets
-{{- $argocdSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-argocd-integration") }}
+{{- $argocdSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-argocd-integration") }}
 {{- $argocdSecretData := ($argocdSecretObj.data | default dict) }}
-{{- $bbSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-bitbucket-integration") }}
-{{- $ghSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-github-integration") }}
-{{- $gitlabSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-gitlab-integration") }}
-{{- $jenkinsSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-jenkins-integration") }}
-{{- $quaySecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-quay-integration") }}
+{{- $bbSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-bitbucket-integration") }}
+{{- $githubSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-github-integration") }}
+{{- $gitlabSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-gitlab-integration") }}
+{{- $jenkinsSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-jenkins-integration") }}
+{{- $quaySecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-quay-integration") }}
 {{- $quaySecretData := ($quaySecretObj.data | default dict) }}
-{{- $nexusSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-nexus-integration") }}
-{{- $artifactorySecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-artifactory-integration") }}
+{{- $nexusSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-nexus-integration") }}
+{{- $artifactorySecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-artifactory-integration") }}
 {{- $artifactorySecretData := ($artifactorySecretObj.data | default dict) }}
 
 {{- if $argocdSecretData }}
@@ -35,25 +36,23 @@ auth:
   environment: production
   providers:
   {{- $signInPage := "" }}
-  {{- $ghSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-github-integration") }}
-  {{- if $ghSecretObj }}
+  {{- if $githubSecretObj }}
     {{- $signInPage = "github" }}
     github:
       production:
         clientId: ${GITHUB__APP__CLIENT__ID}
         clientSecret: ${GITHUB__APP__CLIENT__SECRET}
-      {{- if ne ($ghSecretObj.data.host | b64dec) "github.com" }}
+      {{- if ne ($githubSecretObj.data.host | b64dec) "github.com" }}
         enterpriseInstanceUrl: ${GITHUB__URL}
       {{- end }}
   {{- end }}
-  {{- $glSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-gitlab-integration") }}
-  {{- $glSecretData := ($glSecretObj.data | default dict) }}
-  {{- if $glSecretData }}
-    {{- if and $glSecretData.clientId $glSecretData.clientSecret }}
+  {{- $gitlabSecretData := ($gitlabSecretObj.data | default dict) }}
+  {{- if $gitlabSecretData }}
+    {{- if and $gitlabSecretData.clientId $gitlabSecretData.clientSecret }}
     {{- $signInPage = "gitlab" }}
     gitlab:
       production:
-      {{- if ne ($glSecretData.host | b64dec) "gitlab.com" }}
+      {{- if ne ($gitlabSecretData.host | b64dec) "gitlab.com" }}
         audience: ${GITLAB__URL}
       {{- end }}
         clientId: ${GITLAB__APP__CLIENT__ID}
@@ -90,7 +89,7 @@ integrations:
     - appPassword: ${BITBUCKET__APP_PASSWORD}
       username: ${BITBUCKET__USERNAME}
 {{- end }}
-{{- if $ghSecretObj }}
+{{- if $githubSecretObj }}
   github:
     - host: ${GITHUB__HOST}
       token: ${GITHUB__TOKEN}
@@ -108,7 +107,7 @@ integrations:
       apiBaseUrl: https://${GITLAB__HOST}/api/v4
       token: ${GITLAB__TOKEN}
 {{- end }}
-{{- if (lookup "v1" "Secret" .Release.Namespace "rhtap-jenkins-integration") }}
+{{- if $jenkinsSecretObj }}
 jenkins:
   instances:
     - name: default

installer/charts/rhtap-dh/templates/extra-env.yaml
@@ -1,3 +1,4 @@
+{{- $integrationNamespace := .Values.developerHub.integrationSecrets.namespace -}}
 ---
 apiVersion: v1
 kind: Secret
@@ -13,7 +14,7 @@ data:
     BACKEND_SECRET: {{ randAlphaNum 16 | b64enc }}
     BACKEND_URL: {{ printf "https://backstage-developer-hub-%s.%s" .Release.Namespace .Values.developerHub.ingressDomain | b64enc }}
     NODE_TLS_REJECT_UNAUTHORIZED:  {{ "0" | b64enc }}
-{{- $argocdSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-argocd-integration") }}
+{{- $argocdSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-argocd-integration") }}
 {{- $argocdSecretData := ($argocdSecretObj.data | default dict) }}
 {{- if $argocdSecretData }}
     ARGOCD__API_TOKEN: {{ $argocdSecretData.ARGOCD_API_TOKEN }}
@@ -21,13 +22,13 @@ data:
     ARGOCD__URL: {{ print "https://" ($argocdSecretData.ARGOCD_HOSTNAME | b64dec) | b64enc }}
     ARGOCD__USER: {{ $argocdSecretData.ARGOCD_USER }}
 {{- end }}
-{{- $artifactorySecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-artifactory-integration") }}
+{{- $artifactorySecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-artifactory-integration") }}
 {{- $artifactorySecretData := ($artifactorySecretObj.data | default dict) }}
 {{- if $artifactorySecretData }}
     ARTIFACTORY__API_TOKEN: {{ $artifactorySecretData.token }}
     ARTIFACTORY__URL: {{ $artifactorySecretData.url }}
 {{- end }}
-{{- $bbSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-bitbucket-integration") }}
+{{- $bbSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-bitbucket-integration") }}
 {{- $bbSecretData := ($bbSecretObj.data | default dict) }}
 {{- if $bbSecretData }}
     BITBUCKET__APP_PASSWORD:  {{ $bbSecretData.appPassword }}
@@ -36,7 +37,7 @@ data:
     DEVELOPER_HUB__CATALOG__URL: {{
         required ".developerHub.catalogURL is required" .Values.developerHub.catalogURL | b64enc
     }}
-{{- $ghSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-github-integration") }}
+{{- $ghSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-github-integration") }}
 {{- $ghSecretData := ($ghSecretObj.data | default dict) }}
 {{- if $ghSecretData }}
     GITHUB__APP__ID: {{ $ghSecretData.id }}
@@ -53,7 +54,7 @@ data:
     GITHUB__URL: {{ print "https://" ($ghSecretData.host | b64dec) | b64enc }}
     {{- end }}
 {{- end }}
-{{- $glSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-gitlab-integration") -}}
+{{- $glSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-gitlab-integration") -}}
 {{- $glSecretData := ($glSecretObj.data | default dict) -}}
 {{- if $glSecretData }}
     GITLAB__HOST: {{ $glSecretData.host }}
@@ -64,24 +65,24 @@ data:
     GITLAB__APP__CLIENT__SECRET: {{ $glSecretData.clientSecret }}
     {{- end }}
 {{- end }}
-{{- $jenkinsSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-jenkins-integration") }}
+{{- $jenkinsSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-jenkins-integration") }}
 {{- $jenkinsSecretData := ($jenkinsSecretObj.data | default dict) }}
 {{- if $jenkinsSecretData }}
     JENKINS__BASEURL: {{ $jenkinsSecretData.baseUrl }}
     JENKINS__USERNAME: {{ $jenkinsSecretData.username }}
     JENKINS__TOKEN: {{ $jenkinsSecretData.token }}
 {{- end }}
-{{- $k8sSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-k8s-integration") }}
+{{- $k8sSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-k8s-integration") }}
 {{- $k8sSecretData := ($k8sSecretObj.data | default dict) }}
 {{- if $k8sSecretData }}
     K8S_SERVICEACCOUNT_TOKEN: {{ $k8sSecretData.token }}
 {{- end }}
-{{- $nexusSecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-nexus-integration") }}
+{{- $nexusSecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-nexus-integration") }}
 {{- $nexusSecretData := ($nexusSecretObj.data | default dict) }}
 {{- if $nexusSecretData }}
     NEXUS__URL: {{ $nexusSecretData.url }}
 {{- end }}
-{{- $quaySecretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-quay-integration") }}
+{{- $quaySecretObj := (lookup "v1" "Secret" $integrationNamespace "rhtap-quay-integration") }}
 {{- $quaySecretData := ($quaySecretObj.data | default dict) }}
 {{- if $quaySecretData }}
     {{- if $quaySecretData.token }}

commit cc314c9c713f571c4df27ce57a297ce598432c96

fix: gitops SCM authentication
This was broken with 44728b595a9d9e2b9c04a93e49b2e247d0c45836

rh-pre-commit.version: 2.3.2
rh-pre-commit.check-secrets: ENABLED

installer/charts/rhtap-gitops/values.yaml
@@ -12,6 +12,7 @@ argoCD:
   # The secret namespace to store the ArgoCD API credentials, this secret is later on
   # used for integration with other services.
   integrationSecret:
+    name: __OVERWRITE_ME__
     namespace: __OVERWRITE_ME__
   # Route configuration for all ArgoCD components.
   route:

installer/charts/rhtap-integrations/templates/gitops/bitbucket-auth-secret.yaml
@@ -1,18 +1,0 @@
-{{ $namespace := .Release.Namespace }}
-{{- $secretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-bitbucket-integration") -}}
-{{- $secretData := (get $secretObj "data") | default dict -}}
-{{- if $secretData -}}
-apiVersion: v1
-data:
-  password: {{ $secretData.appPassword }}
-  type: {{ "git" | b64enc }}
-  url: {{ print "https://" ($secretData.host | b64dec) | b64enc }}
-  username: {{ $secretData.username }}
-kind: Secret
-metadata:
-  labels:
-    argocd.argoproj.io/secret-type: repo-creds
-  name: bitbucket-auth-secret
-  namespace: {{ $namespace }}
-type: Opaque
-{{- end }}

installer/charts/rhtap-integrations/templates/gitops/bitbucket-auth.yaml
@@ -1,0 +1,20 @@
+{{- if .Values.argoCD.enabled -}}
+  {{- $secretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-bitbucket-integration") -}}
+  {{- $secretData := (get $secretObj "data") | default dict -}}
+  {{- if $secretData -}}
+apiVersion: v1
+data:
+  password: {{ $secretData.appPassword }}
+  username: {{ $secretData.username }}
+kind: Secret
+metadata:
+  labels:
+    argocd.argoproj.io/secret-type: repo-creds
+  name: bitbucket-auth-secret
+  namespace: {{ .Values.argoCD.namespace }}
+stringData:
+  type: git
+  url: https://{{ $secretData.host | b64dec }}
+type: Opaque
+  {{- end -}}
+{{- end -}}

installer/charts/rhtap-integrations/templates/gitops/github-auth.yaml
@@ -1,0 +1,20 @@
+{{- if .Values.argoCD.enabled -}}
+  {{- $secretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-github-integration") -}}
+  {{- $secretData := (get $secretObj "data") | default dict -}}
+  {{- if $secretData -}}
+apiVersion: v1
+data:
+  password: {{ $secretData.token }}
+kind: Secret
+metadata:
+  labels:
+    argocd.argoproj.io/secret-type: repo-creds
+  name: github-auth-secret
+  namespace: {{ .Values.argoCD.namespace }}
+stringData:
+  type: git
+  url: https://{{ $secretData.host | b64dec }}
+  username: "oauth2"
+type: Opaque
+  {{- end -}}
+{{- end -}}

installer/charts/rhtap-integrations/templates/gitops/gitlab-auth-secret.yaml
@@ -1,18 +1,0 @@
-{{ $namespace := .Release.Namespace }}
-{{- $secretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-gitlab-integration") -}}
-{{- $secretData := (get $secretObj "data") | default dict -}}
-{{- if $secretData -}}
-apiVersion: v1
-data:
-  password: {{ $secretData.token }}
-  type: {{ "git" | b64enc }}
-  url: {{ print "https://" ($secretData.host | b64dec) | b64enc }}
-  username: {{ "oauth2" | b64enc }}
-kind: Secret
-metadata:
-  labels:
-    argocd.argoproj.io/secret-type: repo-creds
-  name: gitlab-auth-secret
-  namespace: {{ $namespace }}
-type: Opaque
-{{- end }}

installer/charts/rhtap-integrations/templates/gitops/gitlab-auth.yaml
@@ -1,0 +1,20 @@
+{{- if .Values.argoCD.enabled -}}
+  {{- $secretObj := (lookup "v1" "Secret" .Release.Namespace "rhtap-gitlab-integration") -}}
+  {{- $secretData := (get $secretObj "data") | default dict -}}
+  {{- if $secretData -}}
+apiVersion: v1
+data:
+  password: {{ $secretData.token }}
+kind: Secret
+metadata:
+  labels:
+    argocd.argoproj.io/secret-type: repo-creds
+  name: gitlab-auth-secret
+  namespace: {{ .Values.argoCD.namespace }}
+stringData:
+  type: git
+  url: https://{{ $secretData.host | b64dec }}
+  username: "oauth2"
+type: Opaque
+  {{- end -}}
+{{- end -}}

installer/charts/rhtap-integrations/values.yaml
@@ -1,6 +1,9 @@
 ---
 integrations:
   acs:
-    enabled: true
+    enabled: __OVERWRITE_ME__
+  argoCD:
+    enabled: __OVERWRITE_ME__
+    namespace: __OVERWRITE_ME__
   quay:
-    enabled: true
+    enabled: __OVERWRITE_ME__

installer/charts/values.yaml.tpl
@@ -263,6 +263,9 @@ quay:
 integrations:
   acs:
     enabled: {{ $acs.Enabled }}
+  argoCD:
+    enabled: {{ $gitops.enabled }}
+    namespace: {{ $gitops.Namespace }}
   quay:
     enabled: {{ $quay.Enabled }}
 #   github:
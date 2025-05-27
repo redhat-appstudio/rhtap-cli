46d68e6c1c184a800e5bb5834a2051732c4b9876
Otávio Fernandes <otaviof@redhat.com>
2025-05-27 22:59:26 +0200

    fix: Splitting TPA and Chicken Realm Charts
    * rename Realm Templates into new chart `tssc-tpa-realm`.
    * move Script and Documentation. Hack script and contributing documentation from `tssc-tpa` moved into
    `tssc-tpa-realm`. These two are dealing with Keycloak Realm things.
    * remove TPA Realm Management from `tssc-tpa`
    * dedicated TPA Realm Management Chart
    * Helm Pre-Install Hook to Wait for SSO Endpoint. Making sure the `tssc-tpa` Helm chart waits for the SSO endpoint before installation and upgrades.
    
    cf RHTAP-5179

---
+++ installer/charts/tssc-tpa-realm/templates/_copy-scripts.tpl
@@ -0,0 +1,1 @@
+../../_common/_copy-scripts.tpl
---
+++ installer/scripts/test-url.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# Test whether the informed URL is online, and returning the expected status code.
+#
+
+shopt -s inherit_errexit
+set -Eeu -o pipefail
+
+# Target URL to test.
+declare -r URL="${URL:-}"
+
+# Expected HTTP status code. Default to 200.
+declare -r STATUS_CODE="${STATUS_CODE:-200}"
+
+#
+# Functions
+#
+
+# Tests if the URL is online and returns the expected HTTP status code.
+probe_url() {
+    local response_code
+
+    echo "# INFO: Probing URL '${URL}' for the status code '${STATUS_CODE}'... "
+
+    # Fetch the HTTP status code from the URL.
+    response_code=$(
+        curl \
+            --silent \
+            --show-error \
+            --fail \
+            --location \
+            --output /dev/null \
+            --write-out "%{http_code}" \
+            "${URL}"
+    )
+    if [[ "${?}" -ne 0 ]]; then
+        echo "# ERROR: Failed to fetch URL '${URL}'."
+        return 1
+    fi
+
+    if [[ "${response_code}" -eq "${STATUS_CODE}" ]]; then
+        echo "# INFO: URL '${URL}' is online and returned '${response_code}'."
+        return 0
+    else
+        echo "# ERROR: '${URL}' returned status code '${response_code}'" \
+            " expected ${STATUS_CODE}."
+        return 1
+    fi
+}
+
+#
+# Main
+#
+
+test_url() {
+    if [[ -z "${URL}" ]]; then
+        echo "# ERROR: URL environment variable is not set." >&2
+        exit 1
+    fi
+
+    if [[ -z "${STATUS_CODE}" ]]; then
+        echo "# ERROR: STATUS_CODE environment variable is not set." >&2
+        exit 1
+    fi
+
+    # Probe the URL until it returns the expected HTTP status code, or exceeds the
+    # retry limit. Each retry waits for a multiple of the previous retry interval.
+    for i in {1..15}; do
+        probe_url &&
+            return 0
+
+        wait=$((i * 3))
+        echo -e "# WARN: [${i}/15] Waiting for ${wait}s before retrying...\n"
+        sleep ${wait}
+    done
+    return 1
+}
+
+if test_url; then
+    echo "# SUCCESS: URL '${URL}' returned expected status code '${STATUS_CODE}'."
+    exit 0
+else
+    echo "# ERROR: URL '${URL}' is not accessible or returned an" \
+        "unexpected status code."
+    exit 1
+fi
